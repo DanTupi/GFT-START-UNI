d87fa5dce6d0bc772defe6293b5d156e7f007328
DanTupi <danilotupinamba@hotmail.com>
2021-02-11 16:17:17 -0300

    last 3 minutes push

--- gft-uni/src/main/java/junit/exercicio3/SimulacaoCustoFrete.java
+++ gft-uni/src/main/java/junit/exercicio3/SimulacaoCustoFrete.java
@@ -1,6 +1,21 @@
 package junit.exercicio3;
 package junit.exercicio2;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class SimulacaoCustoFrete {
+    public static void main(String[] args) {
+        
+        Vagao 1 = new Vagao(27500, 18550);
+
+        Caminhao 1 = new Caminhao(27500, 18550);
+
+        List<Vagao> vagoes = new ArrayList<>();
+        vagoes.add(1);
+
+        List<Caminhao> caminhoes = new ArrayList<>();
+        caminhoes.add(1);
+    }
 
 }
